9e98d8a2d21ad8e36f98f7d330ebf7f1afa07bcc
Yuma-Noguchi <n.yuma39@icloud.com>
2024-03-24 14:33:44 +0000

    Changed viewmodel to observableobject

--- TouchlessWhiteboard/ViewModel/SettingsWindowViewModel.cs
+++ TouchlessWhiteboard/ViewModel/SettingsWindowViewModel.cs
@@ -9,217 +9,50 @@ using System.Runtime.CompilerServices;
 
 namespace TouchlessWhiteboard.ViewModel;
 
-public partial class SettingsWindowViewModel : INotifyPropertyChanged
+public partial class SettingsWindowViewModel : ObservableObject
 {
-    private bool _isTouchlessArtsEnabled;
-    private bool _isEraserEnabled;
-    private bool _isShapesEnabled;
-    private bool _isSelectionEnabled;
-    private bool _isStickyNotesEnabled;
-    private bool _isCameraEnabled;
-    private bool _isSearchEnabled;
-    private bool _isCopilotEnabled;
-    private bool _isToolsEnabled;
-    private bool _isInAir3DMouseEnabled;
+    [ObservableProperty]
+    private bool isTouchlessArtsEnabled;
+    [ObservableProperty]
+    private bool isEraserEnabled;
+    [ObservableProperty]
+    private bool isShapesEnabled;
+    [ObservableProperty]
+    private bool isSelectionEnabled;
+    [ObservableProperty]
+    private bool isStickyNotesEnabled;
+    [ObservableProperty]
+    private bool isCameraEnabled;
+    [ObservableProperty]
+    private bool isSearchEnabled;
+    [ObservableProperty]
+    private bool isCopilotEnabled;
+    [ObservableProperty]
+    private bool isToolsEnabled;
+    [ObservableProperty]
+    private bool isInAir3DMouseEnabled;
 
-    private bool _isLeftHanded;
-    private bool _isRightHanded;
+    [ObservableProperty]
+    private bool isLeftHanded;
+    [ObservableProperty]
+    private bool isRightHanded;
 
-    private int _pinchSensitivity;
+    [ObservableProperty]
+    private int pinchSensitivity;
 
-    private bool _isCalculatorEnabled;
-    private bool _isRulerEnabled;
-    private bool _isTimerEnabled;
-    private bool _isAlarmEnabled;
-    private bool _isQuickFileAccessEnabled;
+    [ObservableProperty]
+    private bool isCalculatorEnabled;
+    [ObservableProperty]
+    private bool isRulerEnabled;
+    [ObservableProperty]
+    private bool isTimerEnabled;
+    [ObservableProperty]
+    private bool isAlarmEnabled;
+    [ObservableProperty]
+    private bool isQuickFileAccessEnabled;
 
     public SettingsWindowViewModel()
     {
     }
-    public bool IsTouchlessArtsEnabled
-    {
-        get { return _isTouchlessArtsEnabled; }
-        set
-        {
-            _isTouchlessArtsEnabled = value;
-            OnPropertyChanged("IsTouchlessArtsEnabled");
-        }
-    }
-
-    public bool IsEraserEnabled
-    {
-        get { return _isEraserEnabled; }
-        set
-        {
-            _isEraserEnabled = value;
-            OnPropertyChanged("IsEraserEnabled");
-        }
-    }
-
-    public bool IsShapesEnabled
-    {
-        get { return _isShapesEnabled; }
-        set
-        {
-            _isShapesEnabled = value;
-            OnPropertyChanged("IsShapesEnabled");
-        }
-    }
-
-    public bool IsSelectionEnabled
-    {
-        get { return _isSelectionEnabled; }
-        set
-        {
-            _isSelectionEnabled = value;
-            OnPropertyChanged("IsSelectionEnabled");
-        }
-    }
-
-    public bool IsStickyNotesEnabled
-    {
-        get { return _isStickyNotesEnabled; }
-        set
-        {
-            _isStickyNotesEnabled = value;
-            OnPropertyChanged("IsStickyNotesEnabled");
-        }
-    }
-
-    public bool IsCameraEnabled
-    {
-        get { return _isCameraEnabled; }
-        set
-        {
-            _isCameraEnabled = value;
-            OnPropertyChanged("IsCameraEnabled");
-        }
-    }
-
-    public bool IsSearchEnabled
-    {
-        get { return _isSearchEnabled; }
-        set
-        {
-            _isSearchEnabled = value;
-            OnPropertyChanged("IsSearchEnabled");
-        }
-    }
-
-    public bool IsCopilotEnabled
-    {
-        get { return _isCopilotEnabled; }
-        set
-        {
-            _isCopilotEnabled = value;
-            OnPropertyChanged("IsCopilotEnabled");
-        }
-    }
-
-    public bool IsToolsEnabled
-    {
-        get { return _isToolsEnabled; }
-        set
-        {
-            _isToolsEnabled = value;
-            OnPropertyChanged("IsToolsEnabled");
-        }
-    }
-
-    public bool IsInAir3DMouseEnabled
-    {
-        get { return _isInAir3DMouseEnabled; }
-        set
-        {
-            _isInAir3DMouseEnabled = value;
-            OnPropertyChanged("IsInAir3DMouseEnabled");
-        }
-    }
-
-    public bool IsLeftHanded
-    {
-        get { return _isLeftHanded; }
-        set
-        {
-            _isLeftHanded = value;
-            OnPropertyChanged("IsLeftHanded");
-        }
-    }
-
-    public bool IsRightHanded
-    {
-        get { return _isRightHanded; }
-        set
-        {
-            _isRightHanded = value;
-            OnPropertyChanged("IsRightHanded");
-        }
-    }
-
-    public int PinchSensitivity
-    {
-        get { return _pinchSensitivity; }
-        set
-        {
-            _pinchSensitivity = value;
-            OnPropertyChanged("PinchSensitivity");
-        }
-    }
-
-    public bool IsCalculatorEnabled
-    {
-        get { return _isCalculatorEnabled; }
-        set
-        {
-            _isCalculatorEnabled = value;
-            OnPropertyChanged("IsCalculatorEnabled");
-        }
-    }
-
-    public bool IsRulerEnabled
-    {
-        get { return _isRulerEnabled; }
-        set
-        {
-            _isRulerEnabled = value;
-            OnPropertyChanged("IsRulerEnabled");
-        }
-    }
-
-    public bool IsTimerEnabled
-    {
-        get { return _isTimerEnabled; }
-        set
-        {
-            _isTimerEnabled = value;
-            OnPropertyChanged("IsTimerEnabled");
-        }
-    }
-
-    public bool IsAlarmEnabled
-    {
-        get { return _isAlarmEnabled; }
-        set
-        {
-            _isAlarmEnabled = value;
-            OnPropertyChanged("IsAlarmEnabled");
-        }
-    }
-
-    public bool IsQuickFileAccessEnabled
-    {
-        get { return _isQuickFileAccessEnabled; }
-        set
-        {
-            _isQuickFileAccessEnabled = value;
-            OnPropertyChanged("IsQuickFileAccessEnabled");
-        }
-    }
-
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
+    
 }
